816fae7b436fa45715a3895f9e6f4b5f8c028272
Ronald Tapias <ronta067@gmail.com>
2026-06-16 16:01:34 -0500

    feat: Add API configuration for Football data integration

---
+++ src/server/config/api-Football.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const apiFootball = axios.create({
+  baseURL: process.env.API_URL,
+  headers: {
+    'x-rapidapi-key': process.env.API_KEY,
+  }
+});
+
+export default apiFootball;
