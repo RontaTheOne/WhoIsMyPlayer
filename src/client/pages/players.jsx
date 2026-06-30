43a0fb4b08db2d0d5745a5e6fa83c944121e99e2
Ronald Tapias <ronta067@gmail.com>
2026-06-29 23:49:34 -0500

    style: Improve code formatting and readability in Players component

--- src/client/pages/players.jsx
+++ src/client/pages/players.jsx
@@ -9,7 +9,7 @@ import PreviewPlayer from "../components/player/previewPlayers.jsx";
 function Players() {
   const [players, setPlayers] = useState([]);
   const [loading, setLoading] = useState(true);
-  const {teamId} = useParams();
+  const { teamId } = useParams();
   console.log("TEAM ID:", teamId);
   useEffect(() => {
     fetch(`http://localhost:3000/world-cup-teams/team/${teamId}`)
@@ -32,20 +32,20 @@ function Players() {
     <div className="players-page">
       <div className="container">
         <TeamHeader />
-        <br/>
+        <br />
         <PositionStats />
         <PlayerFilters />
 
         <h2 className="text-center my-4">Jugadores convocados</h2>
-           <div className="row justify-content-center g-4">
-    {players.map((player) => (
-        <div
-            key={player.id}
-            className="col-sm-12 col-md-6 col-lg-3 d-flex justify-content-center"
-          >
-            <PreviewPlayer player={player} />
-          </div>
-        ))}
+        <div className="row justify-content-center g-4">
+          {players.map((player) => (
+            <div
+              key={player.id}
+              className="col-sm-12 col-md-6 col-lg-3 d-flex justify-content-center"
+            >
+              <PreviewPlayer player={player} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
